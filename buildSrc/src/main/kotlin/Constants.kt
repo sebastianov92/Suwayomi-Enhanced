eda3e5a8f4d52babd2cc63034a1265705875ce66
sebastianov92 <163242380+sebastianov92@users.noreply.github.com>
2026-05-06 07:58:14 -0500

    chore: bundle WebUI r3143

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -12,7 +12,7 @@ const val MainClass = "suwayomi.tachidesk.MainKt"
 // should be bumped with each stable release
 val getTachideskVersion = { "v2.1.${getCommitCount()}" }
 
-val webUIRevisionTag = "r3142"
+val webUIRevisionTag = "r3143"
 
 private val getCommitCount = {
     System.getenv("SUWAYOMI_COMMIT_COUNT")?.takeIf { it.isNotBlank() }
